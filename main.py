4409d0d0b7e300693eecc816e17081f30d024e9e
clayj-cyber <cjb2270@columbia.edu>
2024-08-11 10:27:30 -0400

    Update [main.py] for dungeondungeon directory

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from dungeon.dungeon import Dungeon
+from dungeon.dungeon import Dungeon  # Imports the Dungeon class from the dungeon module within the dungeon directory
 
 if __name__ == "__main__":
     dungeon = Dungeon()
