b86fe9d80502bf19fbe17bd330ab6b2953313c2b
clayj-cyber <cjb2270@columbia.edu>
2024-08-12 12:28:57 -0400

    Delete main.py

--- main.py
+++
@@ -1,5 +0,0 @@
-from dungeon.dungeon import Dungeon  # Imports the Dungeon class from the dungeon module within the dungeon directory
-
-if __name__ == "__main__":
-    dungeon = Dungeon()
-    dungeon.play_game()
